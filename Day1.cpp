f6876225d3f18af2b4e5266ac6cd3b8ae123fc3e
jkapoor18 <jyotikpr999@gmail.com>
2024-02-08 15:15:51 +0530

    c++

---
+++ Day1.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+int main()           //writing myself 
+{
+cout<<"My Name is Jyoti Kapoor \n currently I'm pursuing my Master's in mathematics";
+return 0;
+
+
+}            /* Hello C++ programming Language*/
+
+
+
+#include<iostream>
+using namespace std;
+int main() 
+{
+int a,b;
+a = 4;
+b = 6;
+int sum = a + b;
+cout<<sum;
+
+    return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+int main() 
+{
+int a,b,sub;
+a = 9;
+b = 4;
+sub = a-b;
+cout<<sub;
+
+
+
+}
+
+#include<iostream>
+using namespace std;
+int main() 
+{
+int a,b,remainder;
+a = 9;
+b = 4;
+remainder = a%b;
+cout<<remainder;
+
+
+
+}
+
+#include<iostream>
+using namespace std;
+int main() 
+{
+int a,b,rem;
+cin>>a>>b;
+rem = a%b;
+cout<<rem;
+return 0;
+
+
+
+
+}
+
---
+++ index.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+    int a,b;
+    a=3;
+    b=4;
+    int c=a+b;
+    std::cout<<c;
+    return 0;
+}
