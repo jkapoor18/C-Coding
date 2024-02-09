24eec270e8a85a7ce9239b81c9b50f1794e5adb9
jkapoor18 <jyotikpr999@gmail.com>
2024-02-09 13:46:08 +0530

    programming

--- Day2.cpp
+++ Day2.cpp
@@ -18,3 +18,69 @@ int main()
     
     
 }
+
+//increment & decrement
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,b;
+//   cin>>a>>b;
+//   a++;
+//   b--;
+//   cout<<a<<b;
+cout<<"enter value of a :";
+cin>>a;
+cout<<"enter value of b :";
+cin>>b;
+a++;
+b--;
+cout<<"output is :"<<a<<"\n "<<b;
+   return 0;
+
+
+
+//increment & decrement
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int age;
+  cout<<"enter your age: ";
+  cin>>age;
+  if(age>=18)
+  {
+      cout<<"you are eligible for voting";
+     
+  }
+  else{
+      cout<<"you are not eligible for voting";
+  }
+  
+  
+   return 0;
+
+
+//increment & decrement
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int number;
+  cout<<"enter a number: ";
+  cin>>number;
+  if(number%2==0)
+  {
+      cout<<number<<" is even number";
+     
+  }
+  else{
+      cout<<number<<" is odd number";
+  }
+  
+  
+   return 0;
+    
