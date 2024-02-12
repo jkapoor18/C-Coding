f878e04109d51c42b92c3c12c455d43c577a993e
jkapoor18 <jyotikpr999@gmail.com>
2024-02-12 14:55:24 +0530

    updated

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,48 +1,40 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n;
-    cout<<"Enter a values from 1 to 7: ";
-    cin>>n;
-    switch(n)
-    {
-        case 1: if(n==1)
-        {
-            cout<<"Monday";
-        }
-        break;
-        case 2: if(n==2)
-        {
-            cout<<"Tuesday";
-        }
-        break;
-        case 3: if(n==3)
-        {
-            cout<<"wednesday";
-        }
-        break;
-        case 4: if(n==4)
-        {
-            cout<<"Thursday";
-        }
-        break;
-        case 5: if(n==5)
-        {
-            cout<<"Friday";
-        }
-        break;
-        case 6: if(n==6)
-        {
-            cout<<"Saturday";
-        }
-        break;
-        case 7: if(n==7)
-        {
-            cout<<"Sunday";
-        }
-        break;
+    char operation;
+    double num1, num2, result;
+
+    cout << "Enter an operation (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operation. Please enter a valid operator." << endl;
+            break;
     }
 
     return 0;
